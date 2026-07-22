7a15a25aada7d14d53c160ae64088ac1a94ec0a7
Peter Njiiri <njiiripk@gmial.com>
2026-07-23 01:48:12 +0300

    add about_me.py with prsonal info

---
+++ about_me.py
@@ -0,0 +1,6 @@
+name = "peter "
+course = "Sofware_engineering"
+dream_career = "Software_developer"
+print(f"Hi, I'm {name}")
+print(f"I'm studying {course}")
+print(f"I dream of becoming a {dream_career}")
