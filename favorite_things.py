2157ffc558489f8b0847d70338177c3f6dccb5f7
Peter Njiiri <njiiripk@gmial.com>
2026-07-23 02:04:20 +0300

    Add favorite_things

---
+++ favorite_things.py
@@ -0,0 +1,6 @@
+favorite_languages = ["python", "Javascript"]
+favorite_frameworks = ["Django", "Flask","LangGraph"]
+favorite_tools = ["VS code", "Github","JupyterNotbook"]
+print("My Favorite Things")
+for lang in favorite_languages:
+    print(f" - {lang}")
